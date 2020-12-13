91dde15a39241dffd12d6103ba868b30a1750617
t8haja03 <56733444+t8haja03@users.noreply.github.com>
2020-12-13 13:34:00 +0200

    GoPiGo model update
    GoPiGo.fromJson made null proof

--- flutter_app/security_control/lib/models/gopigo.dart
+++ flutter_app/security_control/lib/models/gopigo.dart
@@ -1,4 +1,3 @@
-//TODO: conform to servers model of GoPiGo
 import 'dart:convert';
 
 class GoPiGo {
@@ -7,40 +6,40 @@ class GoPiGo {
   int _batterylevel;
   String _location;
   bool _connected;
-  Function(int, String) _updateNameCallBack; // If not null, called when setName() is called
+  Function(int, String)
+      _updateNameCallBack; // If not null, called when setName() is called
 
   GoPiGo(this._id, this._name, this._batterylevel);
   GoPiGo.empty();
 
-  GoPiGo.fromJson(String content, this._id, [Function(int, String) updateNameCallBack]){
+  GoPiGo.fromJson(String content, this._id,
+      [Function(int, String) updateNameCallBack]) {
     // Server brings our info in list form unfortunately, so we must do this manually
     // Content =
     List details = jsonDecode(content)[0];
-    this._name = details[0];
-    this._batterylevel = details[1].round();
-    this._location = details[2];
-    if(details[3] == 0) {
+    this._name = details[0] ?? 'GoPiGo_$id';
+    this._batterylevel = details[1]?.round() ?? 0;
+    this._location = details[2] ?? 'lost';
+    if (details[3] == 0) {
       this._connected = false;
-    }
-    else{
+    } else {
       this._connected = true;
     }
-    if(updateNameCallBack != null){
+    if (updateNameCallBack != null) {
       _updateNameCallBack = updateNameCallBack;
     }
   }
 
-  // TODO: REMOVE THIS, TEMPORARY
-  DateTime get getTimestamp => DateTime.now();
   String get name => _name;
   int get id => _id;
   int get batterylevel => _batterylevel;
   bool get connected => _connected;
-  void setId(int i) => _id = i; // TODO: remove this, id should be final?
+  String get location => _location;
+  void setId(int i) => _id = i;
   void setBatteryLevel(int i) => _batterylevel = i;
   void setName(String newName) {
     _name = newName;
-    if(_updateNameCallBack != null){
+    if (_updateNameCallBack != null) {
       _updateNameCallBack(_id, _name);
     }
   }
