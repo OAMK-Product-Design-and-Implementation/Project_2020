763be8d1fa75472e2d04318faf3200b7a58dd3e5
t8haja03 <56733444+t8haja03@users.noreply.github.com>
2020-12-14 13:39:33 +0200

    Update server_settings.dart
    some code formatting,
    removed unused import

--- flutter_app/security_control/lib/pages/settings_page/server_settings.dart
+++ flutter_app/security_control/lib/pages/settings_page/server_settings.dart
@@ -1,4 +1,3 @@
-import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'subsettings_viewmodel.dart';
@@ -11,41 +10,47 @@ class ServerSettingsPage extends StatelessWidget {
         onModelReady: (model) => model.initialise(),
         disposeViewModel: false,
         viewModelBuilder: () => SubSettingsViewModel(),
-        builder: (context, model, child) =>
-            Scaffold(
-                appBar: AppBar(
-                  title: Text(model.serverSettingsLabel),
-                ),
-                body: Card(
-                    child:
-                    ListView(
-                      padding: EdgeInsets.only(top: 8),
-                      children: [
-                        serverAddressTileAnimated(context, model),
-                        Divider(),
-                        //TODO: Preferences are still updated while sliding, maybe the widget is rebuilding?
-                        menuSliderListItem(model.serverUpdateIntervalLabel, context, model.setServerUpdateInterval,
-                            model.serverUpdateInterval, model.maxServerUpdateInterval, model.minServerUpdateInterval,
-                            model.saveServerUpdateInterval()),
-                        FlatButton(onPressed: model.stopSync, child: Text("STOP SYNC")),
-                        FlatButton(onPressed: model.startSync, child: Text("START SYNC"))
-                      ],
-                    )
-                )
-            )
-    );
+        builder: (context, model, child) => Scaffold(
+            appBar: AppBar(
+              title: Text(model.serverSettingsLabel),
+            ),
+            body: Card(
+                child: ListView(
+              padding: EdgeInsets.only(top: 8),
+              children: [
+                serverAddressTileAnimated(context, model),
+                Divider(),
+                //TODO: Preferences are still updated while sliding, maybe the widget is rebuilding?
+                menuSliderListItem(
+                    model.serverUpdateIntervalLabel,
+                    context,
+                    model.setServerUpdateInterval,
+                    model.serverUpdateInterval,
+                    model.maxServerUpdateInterval,
+                    model.minServerUpdateInterval,
+                    model.saveServerUpdateInterval()),
+                FlatButton(onPressed: model.stopSync, child: Text("STOP SYNC")),
+                FlatButton(
+                    onPressed: model.startSync, child: Text("START SYNC"))
+              ],
+            ))));
   }
 }
 
-Widget serverAddressTileAnimated(context, model){
-  return ListTile(title: Text(model.serverAddressLabel),
-      onTap: (){
-        showDialog(context:context,
-          builder: (BuildContext context) => requestTextAlertDialog(context,model.serverAddressDialogTitle, model.serverAddressDialogHintLabel,
-            model.serverAddressDialogConfirmButtonLabel, model.serverAddressDialogCancelButtonLabel,
-            model.setServerAddress, model.serverAddressEditingController));
-        },
-     subtitle: Text(model.serverAddress)
-  );
-
+Widget serverAddressTileAnimated(context, model) {
+  return ListTile(
+      title: Text(model.serverAddressLabel),
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) => requestTextAlertDialog(
+                context,
+                model.serverAddressDialogTitle,
+                model.serverAddressDialogHintLabel,
+                model.serverAddressDialogConfirmButtonLabel,
+                model.serverAddressDialogCancelButtonLabel,
+                model.setServerAddress,
+                model.serverAddressEditingController));
+      },
+      subtitle: Text(model.serverAddress));
 }
