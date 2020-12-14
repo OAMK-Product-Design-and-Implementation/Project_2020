532f90f793b3edf3f87f9c746230e0bf3584061b
t8haja03 <56733444+t8haja03@users.noreply.github.com>
2020-12-14 12:42:16 +0200

    Update local_storage_service.dart
    Removed unused variables and code

--- flutter_app/security_control/lib/services/local_storage_service.dart
+++ flutter_app/security_control/lib/services/local_storage_service.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
-
 
 // Service for storing and retrieving data from SharedPreferences.
 // Used for settings and simple data.
@@ -26,63 +23,43 @@ import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
 //    them in the Json.
 
 class LocalStorageService {
-  static LocalStorageService _instance = null;
-  static StreamingSharedPreferences _preferences = null;
+  static LocalStorageService _instance;
+  static StreamingSharedPreferences _preferences;
 
   static const String ServerUpdateIntervalKey = 'updateinterval';
   static const String ServerAddressKey = 'serveraddress';
 
-
-  static Future<LocalStorageService> getInstance() async{
-    if (_instance == null){
+  static Future<LocalStorageService> getInstance() async {
+    if (_instance == null) {
       _instance = LocalStorageService();
     }
 
-    if(_preferences == null){
+    if (_preferences == null) {
       _preferences = await StreamingSharedPreferences.instance;
     }
     return _instance;
   }
 
-  Preference<int> get serverUpdateInterval => _getFromDisk(ServerUpdateIntervalKey, 5);
-  //set serverUpdateInterval(int value) => _saveToDisk(ServerUpdateIntervalKey, value);
-  Preference<String> get serverAddress => _getFromDisk(ServerAddressKey, "195.148.21.106");
+  Preference<int> get serverUpdateInterval =>
+      _getFromDisk(ServerUpdateIntervalKey, 5);
 
-
-  // //TODO:remove later
-  // Preference<String> get goPiGoExampleString {
-  //   Preference<String> gopigoJson = _getFromDisk(GoPiGoExampleKey, "");
-  //   return gopigoJson;
-  // }
-
-  // GoPiGoExample get goPiGoExample {
-  //   Preference<String> gopigoJson = _getFromDisk(GoPiGoExampleKey, "");
-  //   if(gopigoJson.getValue() == ""){
-  //       return GoPiGoExample(id: "",battery: 0,location: 0);
-  //   }
-  //   return GoPiGoExample.fromJson(json.decode(gopigoJson.getValue()));
-  // }
-  // set goPiGoExample(GoPiGoExample goPiGo){
-  //   _saveToDisk(GoPiGoExampleKey, json.encode(goPiGo.toJson()));
-  // }
+  Preference<String> get serverAddress =>
+      _getFromDisk(ServerAddressKey, "195.148.21.106");
 
   // Generic get method:
   dynamic _getFromDisk(String key, var defaultVal) {
     var value;
-    if(defaultVal is String) {
+    if (defaultVal is String) {
       value = _preferences.getString(key, defaultValue: defaultVal);
-      print('(TRACE) LocalStorageService:_getFromDisk. key: $key value:'+value.getValue());
-    }
-    else if(defaultVal is int){
+      print('(TRACE) LocalStorageService:_getFromDisk. key: $key value:' +
+          value.getValue());
+    } else if (defaultVal is int) {
       value = _preferences.getInt(key, defaultValue: defaultVal);
-    }
-    else if(defaultVal is double){
+    } else if (defaultVal is double) {
       value = _preferences.getDouble(key, defaultValue: defaultVal);
-    }
-    else if(defaultVal is List<String>){
+    } else if (defaultVal is List<String>) {
       value = _preferences.getStringList(key, defaultValue: defaultVal);
-    }
-    else if(defaultVal is bool){
+    } else if (defaultVal is bool) {
       value = _preferences.getBool(key, defaultValue: defaultVal);
     }
     print('(TRACE) LocalStorageService:_getFromDisk. key: $key value: $value');
@@ -90,49 +67,26 @@ class LocalStorageService {
   }
 
   // generic save method:
-  void _saveToDisk<T>(String key, T content){
-    print('(TRACE) LocalStorageService:_saveStringToDisk. key: $key value: $content');
+  void _saveToDisk<T>(String key, T content) {
+    print(
+        '(TRACE) LocalStorageService:_saveStringToDisk. key: $key value: $content');
 
-    if(content is String) {
+    if (content is String) {
       _preferences.setString(key, content);
-      print('(TRACE) LocalStorageService:_saveStringToDisk. key: $key value:' + content);
+      print('(TRACE) LocalStorageService:_saveStringToDisk. key: $key value:' +
+          content);
     }
-    if(content is bool) {
+    if (content is bool) {
       _preferences.setBool(key, content);
     }
-    if(content is int) {
+    if (content is int) {
       _preferences.setInt(key, content);
     }
-    if(content is double) {
+    if (content is double) {
       _preferences.setDouble(key, content);
     }
-    if(content is List<String>) {
+    if (content is List<String>) {
       _preferences.setStringList(key, content);
     }
   }
-
 }
-//
-//
-// // TODO: This is an example of saved data, remove/change
-// // {"id": "unique","battery": -108228463,"location": 1}
-// class GoPiGoExample {
-//   final String id;
-//   final int battery;
-//   final int location;
-//   GoPiGoExample({this.id, this.battery, this.location});
-//
-//   GoPiGoExample.fromJson(Map<String, dynamic> json)
-//       : id = json['id'],
-//         battery = json['battery'],
-//         location = json['location'];
-//
-//   Map<String, dynamic> toJson() {
-//     final Map<String, dynamic> data = new Map<String, dynamic>();
-//     data['id'] = this.id;
-//     data['battery'] = this.battery;
-//     data['location'] = this.location;
-//     return data;
-//   }
-// }
-//
